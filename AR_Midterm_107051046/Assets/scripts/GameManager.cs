b22db8bf0a6d3395be9f3b1617c50e293ecc2e3d
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2020-11-10 01:44:46 +0800

    1110重傳

---
+++ AR_Midterm_107051046/Assets/scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Arisa")]
+    public Transform arisa;
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("旋轉速度"), Range(0.1f, 20f)]
+    public float turn = 1.5f
+    [Header("縮放"), Range(0f, 5f)]
+    public float size = 0.3f
+
+    private void Start()
+    {
+        print("開始事件");
+    }
+
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+
+        arisa.Rotate(0, -joystick.Horizontal * turn, 0);
+       
+        arisa.localScale += new Vector3(1, 1, 1) * joystick.Vertical;
+    }
+}
